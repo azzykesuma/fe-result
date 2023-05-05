d46e198c1845898cbd8591cd6b830c9c08297dc1
Azzzy <official.azzy@gmail.com>
2023-05-05 08:03:49 +0630

    finishing challenge

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const summaryCard = document.getElementById('summary-card');
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    data.forEach(item => {
+    data.forEach((item,index) => {
       const div = document.createElement('div');
       div.className = 'summary_points';
+      div.classList.add(`summary_points_${index}`);
 
       const img = document.createElement('img');
       img.src = item.icon;
@@ -13,9 +14,11 @@ fetch('data.json')
 
       const category = document.createElement('p');
       category.textContent = item.category;
+      category.setAttribute('class','category')
 
       const score = document.createElement('p');
-      score.innerHTML = `<strong>${item.score}</strong>/ 100`;
+      score.innerHTML = `<strong>${item.score}</strong> <span>/ 100</span>`;
+      score.setAttribute('class', 'score_pts')
 
       div.appendChild(img);
       div.appendChild(category);
